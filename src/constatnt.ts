5821a5324ef942a3d1974f91181ba619665330e8
progress2002 <ezeamakachisom2002@gmail.com>
2025-05-07 19:23:07 +0100

    feat: add news and ticket items constants

---
+++ src/constatnt.ts
@@ -0,0 +1,14 @@
+export const newsItems = [
+  "#GBAM Secondary Schools Championship starting next month",
+  "Join our next basketball clinic on July 15th",
+  "GBAM Arena now open for registrations",
+  "New merchandise collection launching soon",
+  "UNICEF partnership extended for 2025",
+];
+
+export const ticketItems = [
+  "Early Bird Tickets for Annual Championship - 20% off until June 30",
+  "VIP Access to GBAM Finals - Limited availability",
+  "Summer Basketball Camp - Register now",
+  "Live Music Performance at GBAM Arena - July 20",
+];
